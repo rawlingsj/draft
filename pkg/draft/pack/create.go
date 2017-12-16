847b92c60e6106dbc40f730b78195dc471844f98
Matthew Fisher <matt.fisher@microsoft.com>
2017-12-15 16:11:36 -0800

    copy all files from pack dir

--- pkg/draft/pack/create.go
+++ pkg/draft/pack/create.go
@@ -2,14 +2,24 @@ package pack
 
 import (
 	"fmt"
+
+	"github.com/otiai10/copy"
 )
 
 // CreateFrom scaffolds a directory with the src pack.
+//
+// This is different from calling
+//
+// pack, _ := pack.FromDir(src)
+// pack.SaveDir(dest)
+//
+// because this function copies *everything* from src to dest.
 func CreateFrom(dest, src string) error {
-	pack, err := FromDir(src)
+	// first do some validation that we are copying from a valid pack directory
+	_, err := FromDir(src)
 	if err != nil {
 		return fmt.Errorf("could not load %s: %s", src, err)
 	}
 
-	return pack.SaveDir(dest)
+	return copy.Copy(src, dest)
 }
