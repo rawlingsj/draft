726fdb1b9f66dbb6744cc759a8c88689b294e886
Matthew Fisher <matt.fisher@microsoft.com>
2018-01-10 15:11:28 -0800

    refactor FromDir() and pack.SaveDir()

--- pkg/draft/pack/create.go
+++ pkg/draft/pack/create.go
@@ -2,24 +2,15 @@ package pack
 
 import (
 	"fmt"
-
-	"github.com/otiai10/copy"
 )
 
 // CreateFrom scaffolds a directory with the src pack.
-//
-// This is different from calling
-//
-// pack, _ := pack.FromDir(src)
-// pack.SaveDir(dest)
-//
-// because this function copies *everything* from src to dest.
 func CreateFrom(dest, src string) error {
 	// first do some validation that we are copying from a valid pack directory
-	_, err := FromDir(src)
+	pack, err := FromDir(src)
 	if err != nil {
 		return fmt.Errorf("could not load %s: %s", src, err)
 	}
 
-	return copy.Copy(src, dest)
+	return pack.SaveDir(dest)
 }
